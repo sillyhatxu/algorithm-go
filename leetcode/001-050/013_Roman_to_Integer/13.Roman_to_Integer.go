b4a9e924d7cdb90d30eff2f292a290d58ab66555
sillyhat <xushikuan1@163.com>
2019-05-02 22:57:38 +0800

    13. Roman to Integer

---
+++ leetcode/001-050/013_Roman_to_Integer/13.Roman_to_Integer.go
@@ -0,0 +1,107 @@
+package main
+
+import "fmt"
+
+/**
+13. Roman to Integer
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+Input: "III"
+Output: 3
+
+Example 2:
+Input: "IV"
+Output: 4
+
+Example 3:
+Input: "IX"
+Output: 9
+
+
+Example 4:
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 5:
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+*/
+
+func getValue(v byte) int {
+	switch v {
+	case 'I':
+		return 1
+	case 'V':
+		return 5
+	case 'X':
+		return 10
+	case 'L':
+		return 50
+	case 'C':
+		return 100
+	case 'D':
+		return 500
+	case 'M':
+		return 1000
+	}
+	return 0
+}
+
+/**
+Runtime: 16 ms, faster than 100.00% of Go online submissions for Roman to Integer.
+Memory Usage: 3 MB, less than 22.00% of Go online submissions for Roman to Integer.
+*/
+func romanToInt(s string) int {
+	h := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	result := 0
+	for i := 0; i < len(s); i++ {
+		current := h[s[i]]
+		next := 0
+		if i+1 < len(s) {
+			next = h[s[i+1]]
+		}
+		if current >= next {
+			result += current
+		} else {
+			result += next - current
+			i++
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(romanToInt("III") == 3)
+	fmt.Println(romanToInt("IV") == 4)
+	fmt.Println(romanToInt("IX") == 9)
+	fmt.Println(romanToInt("LVIII") == 58)
+	fmt.Println(romanToInt("MCMXCIV") == 1994)
+}

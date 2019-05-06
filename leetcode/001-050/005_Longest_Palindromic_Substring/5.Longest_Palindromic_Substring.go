3a64f011393da2a9dc90726ae3322a4cf7694d0b
cookie <xushikuan1@163.com>
2019-05-06 18:08:18 +0800

    5. Longest Palindromic Substring

--- leetcode/001-050/005_Longest_Palindromic_Substring/5.Longest_Palindromic_Substring.go
+++ leetcode/001-050/005_Longest_Palindromic_Substring/5.Longest_Palindromic_Substring.go
@@ -2,28 +2,6 @@ package main
 
 import "fmt"
 
-/**
-5. Longest Palindromic Substring
-
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
-
-Example 1:
-
-Input: "babad"
-Output: "bab"
-Note: "aba" is also a valid answer.
-Example 2:
-
-Input: "cbbd"
-Output: "bb"
-
-给定一个字符串s，找出s中最长的回文子串。可以假设s的最大长度为1000。
-题目简单，但是这里有一个定义
-
-回文：正读反渎都相同的字符串，就是回文，如：aba -> aba;
-
-*/
-
 //最简单的理解方式，不过很现实时间复杂度太高了，LeetCode已经超时
 func longestPalindromeLow(s string) string {
 	if len(s) == 1 || (len(s) == 2 && s[0] == s[1]) {
---
+++ leetcode/001-050/005_Longest_Palindromic_Substring/README.md
@@ -0,0 +1,24 @@
+# 5. Longest Palindromic Substring
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+### Example 1:
+
+```
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+```
+
+### Example 2:
+
+```
+Input: "cbbd"
+Output: "bb"
+```
+
+> 给定一个字符串s，找出s中最长的回文子串。可以假设s的最大长度为1000。
+>
+> 题目简单，但是这里有一个定义
+>
+> 回文：正读反渎都相同的字符串，就是回文，如：aba -> aba;

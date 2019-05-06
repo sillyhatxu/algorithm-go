90210d1ecbdd857570b0f4e92c627ab32d87ad41
cookie <xushikuan1@163.com>
2019-05-06 18:05:14 +0800

    2. Add Two Numbers

--- leetcode/001-050/002_Add_Two_Numbers/2.Add_Two_Numbers.go
+++ leetcode/001-050/002_Add_Two_Numbers/2.Add_Two_Numbers.go
@@ -5,30 +5,6 @@ import (
 	"math"
 )
 
-/**
-
-2. Add Two Numbers
-
-You are given two non-empty linked lists representing two non-negative integers.
-
-The digits are stored in reverse order and each of their nodes contain a single digit.
-
-Add the two numbers and return it as a linked list.
-
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-Example:
-
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
-Explanation: 342 + 465 = 807.
-
-给定两个非负整数的非空链表
-这些数字相反的顺序存储，每个节点包含单独一个数字。
-添加两个数字和将其作为链接列表返回。
-你可以假设这两个数字不是前边是零的数，除了数字0本身。
-*/
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	result := &ListNode{} //返回结果
 	carryBit := 0         //进位标示
---
+++ leetcode/001-050/002_Add_Two_Numbers/README.md
@@ -0,0 +1,27 @@
+# 2. Add Two Numbers
+
+You are given two `non-empty` linked lists representing two non-negative integers.
+
+The digits are stored `in reverse order` and each of their nodes contain a single digit.
+
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+### Example:
+
+```
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+```
+
+### Translate:
+
+> 给定两个非负整数的非空链表
+>
+> 这些数字相反的顺序存储，每个节点包含单独一个数字。
+>
+> 添加两个数字和将其作为链接列表返回。
+>
+> 你可以假设这两个数字不是前边是零的数，除了数字0本身。

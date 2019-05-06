98323f9eb5aa730c4a7b7edfb23c0ce2e9a3edc4
cookie <xushikuan1@163.com>
2019-05-06 18:03:38 +0800

    1. Two Sum

--- leetcode/001-050/001_Two_Sum/1.Two_Sum.go
+++ leetcode/001-050/001_Two_Sum/1.Two_Sum.go
@@ -2,24 +2,6 @@ package main
 
 import "fmt"
 
-/**
-1. Two Sum
-
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-
-给定一个int数组，返回数组重点的两个数字加和等于目标target。
-你可以假定每一次输入恰好有一个结果，你不可以使用相同的元素两次
-
-*/
 func twoSumBasic(nums []int, target int) []int {
 	for i, n := range nums {
 		for j, n2 := range nums {

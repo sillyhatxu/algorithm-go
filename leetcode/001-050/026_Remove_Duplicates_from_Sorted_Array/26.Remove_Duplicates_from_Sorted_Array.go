5baf5291d19147d50e9b63582cc1e9c162d58fcf
cookie <xushikuan1@163.com>
2019-05-07 15:13:29 +0800

    26. Remove Duplicates from Sorted Array

--- leetcode/001-050/026_Remove_Duplicates_from_Sorted_Array/26.Remove_Duplicates_from_Sorted_Array.go
+++ leetcode/001-050/026_Remove_Duplicates_from_Sorted_Array/26.Remove_Duplicates_from_Sorted_Array.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func removeDuplicates(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	}
 	for i := 1; i < len(nums); i++ {
 		if nums[i-1] == nums[i] {
 			nums = append(nums[:i], nums[i+1:]...)

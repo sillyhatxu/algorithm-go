988f9156d51814e1cdc7f606daa90f2e4e9b5d03
cookie <xushikuan1@163.com>
2019-05-07 13:56:14 +0800

    20. Valid Parentheses

--- leetcode/001-050/020_Valid_Parentheses/20.Valid_Parentheses.go
+++ leetcode/001-050/020_Valid_Parentheses/20.Valid_Parentheses.go
@@ -3,38 +3,6 @@ package main
 import "fmt"
 
 /**
-20. Valid Parentheses
-Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-An input string is valid if:
-
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Note that an empty string is also considered valid.
-
-Example 1:
-
-Input: "()"
-Output: true
-Example 2:
-
-Input: "()[]{}"
-Output: true
-Example 3:
-
-Input: "(]"
-Output: false
-Example 4:
-
-Input: "([)]"
-Output: false
-Example 5:
-
-Input: "{[]}"
-Output: true
-*/
-
-/**
 Runtime: 0 ms, faster than 100.00% of Go online submissions for Valid Parentheses.
 Memory Usage: 2 MB, less than 100.00% of Go online submissions for Valid Parentheses.
 Next challenges:

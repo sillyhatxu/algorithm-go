b328af6f5b73daf476c00f9ae9b26566fa1db8f2
cookie <xushikuan1@163.com>
2019-05-09 19:23:52 +0800

    38. Count and Say

--- leetcode/001-050/038_Count_and_Say/38.Count_and_Say.go
+++ leetcode/001-050/038_Count_and_Say/38.Count_and_Say.go
@@ -23,15 +23,46 @@ func countAndSay(n int) string {
 	return result
 }
 
+func countAndSayOptimize(n int) string {
+	if n == 1 {
+		return "1"
+	}
+	previous := countAndSay(n - 1)
+	result, length := []byte{}, 1
+	for i := 0; i < len(previous)-1; i++ {
+		if previous[i] != previous[i+1] {
+			result = append(result, []byte(strconv.Itoa(length))...)
+			result = append(result, previous[i])
+			length = 1
+		} else {
+			length++
+		}
+	}
+	result = append(result, []byte(strconv.Itoa(length))...)
+	result = append(result, previous[len(previous)-1])
+	return string(result)
+}
+
 func main() {
-	fmt.Println(countAndSay(1) == "1")
-	fmt.Println(countAndSay(2) == "11")
-	fmt.Println(countAndSay(3) == "21")
-	fmt.Println(countAndSay(4) == "1211")
-	fmt.Println(countAndSay(5) == "111221")
-	fmt.Println(countAndSay(6) == "312211")
-	fmt.Println(countAndSay(7) == "13112221")
-	fmt.Println(countAndSay(8) == "1113213211")
-	fmt.Println(countAndSay(9) == "31131211131221")
-	fmt.Println(countAndSay(10) == "13211311123113112211")
+	fmt.Println(countAndSayOptimize(1) == "1")
+	fmt.Println(countAndSayOptimize(2) == "11")
+	fmt.Println(countAndSayOptimize(3) == "21")
+	fmt.Println(countAndSayOptimize(4) == "1211")
+	fmt.Println(countAndSayOptimize(5) == "111221")
+	fmt.Println(countAndSayOptimize(6) == "312211")
+	fmt.Println(countAndSayOptimize(7) == "13112221")
+	fmt.Println(countAndSayOptimize(8) == "1113213211")
+	fmt.Println(countAndSayOptimize(9) == "31131211131221")
+	fmt.Println(countAndSayOptimize(10) == "13211311123113112211")
+
+	//fmt.Println(countAndSay(1) == "1")
+	//fmt.Println(countAndSay(2) == "11")
+	//fmt.Println(countAndSay(3) == "21")
+	//fmt.Println(countAndSay(4) == "1211")
+	//fmt.Println(countAndSay(5) == "111221")
+	//fmt.Println(countAndSay(6) == "312211")
+	//fmt.Println(countAndSay(7) == "13112221")
+	//fmt.Println(countAndSay(8) == "1113213211")
+	//fmt.Println(countAndSay(9) == "31131211131221")
+	//fmt.Println(countAndSay(10) == "13211311123113112211")
 }

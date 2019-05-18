2a84802fb5c359bedcf3e1986b39ff5a1969144f
sillyhat <xushikuan1@163.com>
2019-05-18 23:30:56 +0800

    11. Container With Most Water

--- leetcode/001-050/011_Container_With_Most_Water/11.Container_With_Most_Water.go
+++ leetcode/001-050/011_Container_With_Most_Water/11.Container_With_Most_Water.go
@@ -1,10 +1,47 @@
 package main
 
-func maxArea(height []int) int {
+import "fmt"
 
-	return 0
+func min(i, j int) int {
+	if i > j {
+		return j
+	}
+	return i
+}
+
+/**
+Runtime: 320 ms, faster than 24.67% of Go online submissions for Container With Most Water.
+Memory Usage: 5.6 MB, less than 47.91% of Go online submissions for Container With Most Water.
+*/
+func maxArea(height []int) int {
+	maxArea := 0
+	for i, v := range height {
+		for j := i + 1; j < len(height); j++ {
+			currentArea := min(v, height[j]) * (j - i)
+			if maxArea < currentArea {
+				maxArea = currentArea
+			}
+		}
+	}
+	return maxArea
+}
+
+func maxAreaOptimize(height []int) int {
+	maxArea, start, end := 0, 0, len(height)-1
+	for start != end {
+		currentArea := min(height[end], height[start]) * (end - start)
+		if currentArea > maxArea {
+			maxArea = currentArea
+		}
+		if height[end] > height[start] {
+			start++
+		} else {
+			end--
+		}
+	}
+	return maxArea
 }
 
 func main() {
-
+	fmt.Println(maxAreaOptimize([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}) == 49)
 }

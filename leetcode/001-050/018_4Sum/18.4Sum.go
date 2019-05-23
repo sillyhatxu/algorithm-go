c55f98740e8cb346b56b2055e92765447843ffbc
cookie <xushikuan1@163.com>
2019-05-23 18:31:17 +0800

    18. 4Sum

--- leetcode/001-050/018_4Sum/18.4Sum.go
+++ leetcode/001-050/018_4Sum/18.4Sum.go
@@ -59,6 +59,5 @@ func fourSum(nums []int, target int) [][]int {
 
 func main() {
 	fmt.Println(fourSum([]int{-3, -2, -1, 0, 0, 1, 2, 3}, 0))
-	fmt.Println("[[-3,-2,2,3],[-3,-1,1,3],[-3,0,0,3],[-3,0,1,2],[-2,-1,0,3],[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]")
 	fmt.Println(fourSum([]int{1, 0, -1, 0, -2, 2}, 0))
 }

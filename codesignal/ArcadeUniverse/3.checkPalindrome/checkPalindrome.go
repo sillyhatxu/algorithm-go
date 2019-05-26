93cd7a3128ea3a8ee07efc951aa68faae3298731
sillyhat <xushikuan1@163.com>
2019-05-26 21:49:48 +0800

    3. checkPalindrome

--- codesignal/ArcadeUniverse/3.checkPalindrome/checkPalindrome.go
+++ codesignal/ArcadeUniverse/3.checkPalindrome/checkPalindrome.go
@@ -10,8 +10,20 @@ func checkPalindrome(inputString string) bool {
 	return palindrome == inputString
 }
 
+func checkPalindromeOptimize(s string) bool {
+	return s == Reverse(s)
+}
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
 func main() {
-	fmt.Println(checkPalindrome("aabaa") == true)
+	fmt.Println(checkPalindromeOptimize("aabaa") == true)
 	fmt.Println(checkPalindrome("abac") == false)
 	fmt.Println(checkPalindrome("a") == true)
 	fmt.Println(checkPalindrome("az") == false)

1d07cd99451f86cb17cfbe4d1f5aa93bbd1698ca
cookie <xushikuan1@163.com>
2019-05-31 19:01:25 +0800

    31. Next Permutation error

--- leetcode/001-050/031_Next_Permutation/31.Next_Permutation.go
+++ leetcode/001-050/031_Next_Permutation/31.Next_Permutation.go
@@ -1,9 +1,26 @@
 package main
 
-func nextPermutation(nums []int) {
+import "fmt"
 
+func nextPermutation(nums []int) {
+	for i := len(nums) - 2; i >= 0; i-- {
+		if nums[i] < nums[i+1] {
+			for j := len(nums) - 1; j > i; j-- {
+				if nums[j] > nums[i] {
+					nums[i], nums[j] = nums[j], nums[i]
+					return
+				}
+			}
+		}
+	}
 }
 
 func main() {
-
+	var array []int
+	array = []int{1, 3, 2}
+	nextPermutation(array)
+	fmt.Println(array)
+	//array = []int{1, 2, 3}
+	//nextPermutation(array)
+	//fmt.Println(array)
 }
--- leetcode/001-050/031_Next_Permutation/README.md
+++ leetcode/001-050/031_Next_Permutation/README.md
@@ -13,3 +13,7 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
 ```
+
+### Translate:
+
+> 给定一个数组 [1,2,3] 这个数组可以组成一个数字 123，把可以组成的所有的数字，按照从小到大排列，123这个数字的下一个数字就是132
